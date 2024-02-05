c7394bb847846c5f00c07ec614027791ee6a5fcc
AkhmadKholmurodov <seehyuk2000@gmail.com>
2024-02-04 23:03:17 -0800

    First time of learning react native

--- App.js
+++ App.js
@@ -4,17 +4,39 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box1}></View>
+      <View style={styles.box2}>
+        <Text style={styles.text}>Akhmad Here</Text>
+      </View>
+      <View style={styles.box3}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  box1:{
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  box2:{
+    flex: 2,
+    backgroundColor: '#333',
+    flexGrow: 24,
+  },
+  text: {
+    fontSize: 20,
+    marginHorizontal: 10,
+    color:'#fff',
+    margin: 'auto',
+    position:'relative'
+  },
+  box3:{
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+
+  
 });
